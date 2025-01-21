bd8189ebc7067194de362de70be75de4fa354da8
Udean777 <uaart123@gmail.com>
2025-01-21 19:33:43 +0700

    CHNG: Remove Hive dependencies, add Drift Postgres support, update .env file, and clean up home page imports

--- lib/presentation/home/home_page.dart
+++ lib/presentation/home/home_page.dart
@@ -1,7 +1,5 @@
-import 'package:drift_db_viewer/drift_db_viewer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:the_habits/core/database/database.dart';
 import 'package:the_habits/core/providers/daily_summary_provider.dart';
 import 'package:the_habits/presentation/home/widgets/daily_summary_card.dart';
 import 'package:the_habits/presentation/home/widgets/habit_card_list.dart';
@@ -16,7 +14,6 @@ class HomePage extends HookConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final selectedDate = useState(DateTime.now());
     final colorScheme = Theme.of(context).colorScheme;
-    final db = AppDatabase();
 
     return Scaffold(
       appBar: AppBar(
@@ -25,13 +22,6 @@ class HomePage extends HookConsumerWidget {
           style: TextStyle(
             fontWeight: FontWeight.bold,
           ),
-        ),
-        leading: GestureDetector(
-          onTap: () {
-            Navigator.of(context).push(
-                MaterialPageRoute(builder: (context) => DriftDbViewer(db)));
-          },
-          child: Icon(Icons.data_array),
         ),
       ),
       body: SafeArea(
