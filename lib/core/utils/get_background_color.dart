ad1928a29ed73f4d7c0826713de1fb53b32cfe6f
Udean777 <uaart123@gmail.com>
2025-02-04 08:16:01 +0700

    just commit

--- lib/core/utils/get_background_color.dart
+++ lib/core/utils/get_background_color.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 
 Color getBackgroundColor(String title) {
   final colors = [
-    Colors.purple.withValues(alpha: 0.3),
-    Colors.blue.withValues(alpha: 0.3),
-    Colors.green.withValues(alpha: 0.3),
-    Colors.orange.withValues(alpha: 0.3),
+    Colors.purple.withAlpha(76),
+    Colors.blue.withAlpha(76),
+    Colors.green.withAlpha(76),
+    Colors.orange.withAlpha(76),
+    Colors.red.withAlpha(76),
+    Colors.yellow.withAlpha(76),
   ];
 
   return colors[title.hashCode % colors.length];
